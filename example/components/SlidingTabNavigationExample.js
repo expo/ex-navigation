9b171a75ec5ca98e49713130783fd4f445cdad4e
Satyajit Sahoo <satyajit.happy@gmail.com>
2016-10-28 00:57:29 +0000

    Add example for jumpToTab
    Closes #130
    
    fbshipit-source-id: 504fcc7

--- example/components/SlidingTabNavigationExample.js
+++ example/components/SlidingTabNavigationExample.js
@@ -3,11 +3,13 @@ import {
   StyleSheet,
   View,
   Text,
+  TouchableOpacity,
 } from 'react-native';
 import {
   SlidingTabNavigation,
   SlidingTabNavigationItem,
 } from '@exponent/ex-navigation';
+import { Ionicons } from '@exponent/vector-icons';
 
 export default class SlidingTabNavigationExample extends Component {
   static route = {
@@ -16,6 +18,18 @@ export default class SlidingTabNavigationExample extends Component {
       ...SlidingTabNavigation.navigationBarStyles,
     },
   }
+
+  _goToFirstTab = () => {
+    this.props.navigation.performAction(({ tabs, stacks }) => {
+      tabs('sliding-tab-navigation').jumpToTab('first');
+    });
+  };
+
+  _goToSecondTab = () => {
+    this.props.navigation.performAction(({ tabs, stacks }) => {
+      tabs('sliding-tab-navigation').jumpToTab('second');
+    });
+  };
 
   _renderLabel = ({route}) => {
     let title;
@@ -32,8 +46,8 @@ export default class SlidingTabNavigationExample extends Component {
     return (
       <View style={styles.container}>
         <SlidingTabNavigation
-          id="tab-navigation"
-          navigatorUID="tab-navigation"
+          id="sliding-tab-navigation"
+          navigatorUID="sliding-tab-navigation"
           initialTab="first"
           renderLabel={this._renderLabel}
           barBackgroundColor="#0084FF"
@@ -43,6 +57,9 @@ export default class SlidingTabNavigationExample extends Component {
               <Text style={styles.quoteMarks}>“</Text>
               <Text style={styles.quoteText}>R2D2, you know better than to trust a strange computer!</Text>
               <Text style={styles.quoteAuthor}>C3PO</Text>
+              <TouchableOpacity onPress={this._goToSecondTab}>
+                <Ionicons name="md-arrow-forward" size={16} style={styles.button} />
+              </TouchableOpacity>
             </View>
           </SlidingTabNavigationItem>
           <SlidingTabNavigationItem id="second">
@@ -50,6 +67,9 @@ export default class SlidingTabNavigationExample extends Component {
               <Text style={styles.quoteMarks}>“</Text>
               <Text style={styles.quoteText}>The best thing about a boolean is even if you are wrong, you are only off by a bit.</Text>
               <Text style={styles.quoteAuthor}>Bryan</Text>
+              <TouchableOpacity onPress={this._goToFirstTab}>
+                <Ionicons name="md-arrow-back" size={16} style={styles.button} />
+              </TouchableOpacity>
             </View>
           </SlidingTabNavigationItem>
         </SlidingTabNavigation>
@@ -102,6 +122,11 @@ const styles = StyleSheet.create({
     color: '#888',
     fontSize: 12,
     fontStyle: 'italic',
+  },
+
+  button: {
+    margin: 16,
+    color: '#0084FF',
   },
 
   selectedTab: {
