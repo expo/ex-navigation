f836dbc082fef6ecbdd25a9f409950e54ed8bc41
Janic Duplessis <janicduplessis@gmail.com>
2016-12-17 17:59:27 +0000

    Only transition SharedElements that are in both groups
    There was a bug when having different SharedElements inside the transitioning SharedElementGroups where some would still appear even if they were not in the initial screen that is being transitioned from. This just make sure elements are present in both groups before rendering them in the overlay.
    
    Closes exponentjs/ex-navigation#318
    
    fbshipit-source-id: 09716ca

--- src/shared-element/ExNavigationSharedElementOverlay.js
+++ src/shared-element/ExNavigationSharedElementOverlay.js
@@ -101,9 +101,14 @@ export default class SharedElementOverlay extends React.Component {
     const transitioningFromElementGroup = this.state.elementGroups[this.state.transitioningElementGroupFromUid];
     const transitioningToElementGroup = this.state.elementGroups[this.state.transitioningElementGroupToUid];
 
+    // Only transition elements that are present in both transition groups.
+    const commonElements = transitioningToElementGroup.elements.filter(e1 =>
+      transitioningFromElementGroup.elements.some(e2 => e1.props.id === e2.props.id)
+    );
+
     return (
       <View style={styles.overlay}>
-        {transitioningToElementGroup.elements.map((e, i) => {
+        {commonElements.map((e, i) => {
           const fromMetrics = transitioningFromElementGroup.elementMetrics[e.props.id];
           const toMetrics = transitioningToElementGroup.elementMetrics[e.props.id];
           if (!toMetrics) {
